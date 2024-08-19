f103d25f45797632164549b794f225f05e90d9cf
Elvis <elvis.omondi@student.moringaschool.com>
2024-08-19 11:01:08 +0300

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -29,8 +29,8 @@ function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
