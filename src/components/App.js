6e711a7efb324ee0746bea91775c049b9fe89e72
Elvis <elvis.omondi@student.moringaschool.com>
2024-09-12 17:12:20 +0300

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,31 +3,6 @@ import NavBar from './NavBar';
 import Home from './Home';
 import About from './About';
 
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
-}
-
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
-}
-
-function About() {
-  return (
-    <div id="home">
-      <h2>About</h2>
-    </div>
-  );
-}
-
-
 function App() {
   return (
     <div>
---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+// NavBar.js
+import React from 'react';
+
+function NavBar() {
+  return (
+    <nav>
+      <a href="#home">I'm a link!</a>
+    </nav>
+  );
+}
+
+export default NavBar;
